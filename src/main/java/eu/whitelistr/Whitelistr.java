5e1700bd78f789f7a15705cf208b38df5b0983f1
Avalanche7CZ <avalanche752@gmail.com>
2025-01-01 12:29:48 +0100

    init
    - gradle setup

---
+++ src/main/java/eu/whitelistr/Whitelistr.java
@@ -0,0 +1,14 @@
+package eu.whitelistr;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Whitelistr.MODID, version = "1.0", name = "Interlink", acceptableRemoteVersions = "*")
+public class Whitelistr {
+
+    public static final String MODID = "whitelistr";
+
+
+
+
+
+}
