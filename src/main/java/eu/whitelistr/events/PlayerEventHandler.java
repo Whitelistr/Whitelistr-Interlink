1d2e0bba45097afd7383fcb7c7c5faf997faddbb
Avalanche7CZ <avalanche752@gmail.com>
2025-02-10 17:33:49 +0100

    Revert "Update PlayerEventHandler.java"
    This reverts commit d4bcdd6a433aee58e2e1e0bcc3eba77bdced91b7.

--- src/main/java/eu/whitelistr/events/PlayerEventHandler.java
+++ src/main/java/eu/whitelistr/events/PlayerEventHandler.java
@@ -27,7 +27,7 @@ public class PlayerEventHandler {
     }
 
     @SubscribeEvent
-    public void onPlayerLogin(PlayerEvent.Login event) {
+    public void onPlayerLogin(PlayerEvent.Login event) { // Changed to PlayerEvent.Login
         if (event.player instanceof EntityPlayerMP) {
             NetworkManager networkManager = ((EntityPlayerMP) event.player).playerNetServerHandler.netManager;
             InetSocketAddress remoteAddress = (InetSocketAddress) networkManager.getSocketAddress();
